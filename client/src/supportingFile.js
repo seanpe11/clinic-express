b2c0de97746eba8c21ee33fe9815d9289b789d7b
ludennis-dlsu <dennis_lu@dlsue.edu.ph>
2020-09-08 14:45:25 +0800

    Update supportingFile.js

--- client/src/supportingFile.js
+++ client/src/supportingFile.js
@@ -2,10 +2,18 @@ import axios from 'axios'
 
 const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
+
 const url = 'http://localhost:5000/api/files';
+const collection = db.collection('uploads.files');
+const collectionChunks = db.collection('uploads.chunks');
 
 // Create a storage object with a configuration 
-const storage = new GridFsStorage({ url });
+const storage = new GridFsStorage({
+    url: url,
+    file: (req, file) => {
+        if
+    } 
+});
 
 // Set multer storage engine to the newly created object
 const upload = multer({ storage });
