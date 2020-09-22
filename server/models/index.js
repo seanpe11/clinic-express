96eb2bb188062cc3962f36131a1bd9ba0122b53e
ludennis-dlsu <dennis_lu@dlsue.edu.ph>
2020-09-22 20:39:18 +0800

    fix deleted index

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,7 @@
-'use strict';
-
 module.exports = Object.freeze({
     patients: require('./patients'),
     visits: require('./visits'),
+    files: require('./files'),
     users: require('./users'),
     // files: require('./files'),
 })
